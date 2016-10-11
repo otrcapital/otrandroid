104c8eee219f574443350d2f498780155e32e211
Vladimir Tanakov <Uladzimir.Tanakou@Softensity.com>
2016-10-11 11:07:48 +0300

    Fix NPE in CropImage class

--- app/src/main/java/com/mobile/otrcapitalllc/Activities/CropImage.java
+++ app/src/main/java/com/mobile/otrcapitalllc/Activities/CropImage.java
@@ -1,6 +1,13 @@
 package com.mobile.otrcapitalllc.Activities;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -10,6 +17,7 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
@@ -31,21 +39,19 @@ import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.utils.Converters;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.util.ArrayList;
-import java.util.List;
-
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class CropImage extends Activity
-{
-    @Bind(R.id.editImageView)ImageView editImageView;
-    @Bind(R.id.executeBtn)ImageButton executeBtn;
-    @Bind(R.id.undoBtn)ImageButton undoBtn;
-    @Bind(R.id.EdgeCornersDV)DrawView EdgeCorners;
+public class CropImage extends Activity {
+    @Bind(R.id.editImageView)
+    ImageView editImageView;
+    @Bind(R.id.executeBtn)
+    ImageButton executeBtn;
+    @Bind(R.id.undoBtn)
+    ImageButton undoBtn;
+    @Bind(R.id.EdgeCornersDV)
+    DrawView EdgeCorners;
     Bitmap origBitmap, warpedBitmap;
     private String imagePath;
     private String cropDone = "crop";
@@ -68,28 +74,22 @@ public class CropImage extends Activity
     };
 
     @OnClick(R.id.executeBtn)
-    public void execute (View view)
-    {
-        if(cropDone == "crop")
-        {
-            Bitmap bitmap = ((BitmapDrawable)editImageView.getDrawable()).getBitmap();
+    public void execute(View view) {
+        if (cropDone.equals("crop")) {
+            Bitmap bitmap = ((BitmapDrawable) editImageView.getDrawable()).getBitmap();
             AsyncTaskWarp warp = new AsyncTaskWarp();
             warp.execute(bitmap);
-        }
-
-        else if(cropDone == "done")
-        {
+        } else if (cropDone.equals("done")) {
             saveOutput();
             Intent returnIntent = new Intent();
-            setResult(RESULT_CANCELED,returnIntent);
+            setResult(RESULT_CANCELED, returnIntent);
             finish();
         }
 
     }
 
     @OnClick(R.id.undoBtn)
-    public void undo (View view)
-    {
+    public void undo(View view) {
         undoBtn.setEnabled(false);
         editImageView.setImageBitmap(origBitmap);
         undoBtn.setAlpha(0.5f);
@@ -100,8 +100,7 @@ public class CropImage extends Activity
     }
 
     @OnClick(R.id.brightnessContrastBtn)
-    public void launchBrightnessContrast (View view)
-    {
+    public void launchBrightnessContrast(View view) {
         saveOutput();
         Intent returnIntent = new Intent();
         setResult(RESULT_OK, returnIntent);
@@ -109,60 +108,71 @@ public class CropImage extends Activity
     }
 
     @OnClick(R.id.rotateBtn)
-    public void rotateBtn (View view)
-    {
+    public void rotateBtn(View view) {
         rotateBitmap(180);
         editImageView.setImageBitmap(origBitmap);
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Bundle extras = getIntent().getExtras();
-        imagePath = extras.getString("image_path");
+        setContentView(R.layout.activity_crop_image);
+        ButterKnife.bind(this);
+
+        imagePath = getIntent().getStringExtra("image_path");
         origBitmap = BitmapFactory.decodeFile(imagePath);
-        if (origBitmap.getWidth() > origBitmap.getHeight()){
+        if (origBitmap == null) {
+            showErrorDialog();
+        }
+        if (origBitmap.getWidth() > origBitmap.getHeight()) {
             rotateBitmap(90);
         }
-        setContentView(R.layout.activity_crop_image);
-       // LinearLayout progressIndicatorGroup = (LinearLayout)findViewById(R.id.progressIndicatorGroup);
-       // TextView verifyUserTV = (TextView) findViewById(R.id.verifyUserTV);
-        //verifyUserTV.setText("Loading...\nPlease wait");
-        //progressIndicatorGroup.setVisibility(View.VISIBLE);
+
         if (!OpenCVLoader.initDebug()) {
             OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
         }
-        ButterKnife.bind(this);
         editImageView.setImageBitmap(origBitmap);
-        //progressIndicatorGroup.setVisibility(View.INVISIBLE);
-        //verifyUserTV.setText("Cropping image...");
     }
 
-    private void rotateBitmap(int rotation)
-    {
+    private void showErrorDialog() {
+        AlertDialog.Builder adb = new AlertDialog.Builder(this);
+        adb.setTitle("Error");
+        adb.setMessage("Cannot open file from path");
+        adb.setIcon(android.R.drawable.ic_dialog_alert);
+        adb.setPositiveButton("OK", new OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                Intent returnIntent = new Intent();
+                setResult(RESULT_CANCELED, returnIntent);
+                finish();
+            }
+        });
+        adb.create().show();
+    }
+
+    private void rotateBitmap(int rotation) {
         int width = origBitmap.getWidth();
         int height = origBitmap.getHeight();
         Matrix matrix = new Matrix();
         matrix.preRotate(rotation);
-        Log.d(ActivityTags.TAG_LOG + "Image","Image width: " + width + " Height: " + height);
+        Log.d(ActivityTags.TAG_LOG + "Image", "Image width: " + width + " Height: " + height);
         Bitmap adjustedBitmap = Bitmap.createBitmap(origBitmap, 0, 0, width, height, matrix, true);
-        origBitmap=adjustedBitmap;
+        origBitmap = adjustedBitmap;
     }
 
-    private Bitmap warp(Bitmap image, Point topLeft, Point topRight, Point bottomLeft, Point bottomRight)
-    {
-        int resultWidth = (int)(topRight.x - topLeft.x);
-        int bottomWidth = (int)(bottomRight.x - bottomLeft.x);
-        if(bottomWidth > resultWidth)
+    private Bitmap warp(Bitmap image, Point topLeft, Point topRight, Point bottomLeft, Point bottomRight) {
+        int resultWidth = (int) (topRight.x - topLeft.x);
+        int bottomWidth = (int) (bottomRight.x - bottomLeft.x);
+        if (bottomWidth > resultWidth)
             resultWidth = bottomWidth;
 
-        int resultHeight = (int)(bottomLeft.y - topLeft.y);
-        int bottomHeight = (int)(bottomRight.y - topRight.y);
-        if(bottomHeight > resultHeight)
+        int resultHeight = (int) (bottomLeft.y - topLeft.y);
+        int bottomHeight = (int) (bottomRight.y - topRight.y);
+        if (bottomHeight > resultHeight)
             resultHeight = bottomHeight;
 
-        resultHeight = (int) Math.round(resultWidth *1.41);
+        resultHeight = (int) Math.round(resultWidth * 1.41);
 
         Mat inputMat = new Mat(image.getHeight(), image.getHeight(), CvType.CV_8UC1);
         Utils.bitmapToMat(image, inputMat);
@@ -195,8 +205,7 @@ public class CropImage extends Activity
         return output;
     }
 
-    private Point[] ScreenPoint2ImagePixels(android.graphics.Point[] ScreenPoints)
-    {
+    private Point[] ScreenPoint2ImagePixels(android.graphics.Point[] ScreenPoints) {
         Drawable drawable = editImageView.getDrawable();
         Rect imageBounds = drawable.getBounds();
 
@@ -209,12 +218,11 @@ public class CropImage extends Activity
         int scaledHeight = editImageView.getMeasuredHeight();
 
         //Find the ratio of the original image to the scaled image
-        double heightRatio = (double)intrinsicHeight / (double)scaledHeight;
-        double widthRatio = (double)intrinsicWidth / (double)scaledWidth;
+        double heightRatio = (double) intrinsicHeight / (double) scaledHeight;
+        double widthRatio = (double) intrinsicWidth / (double) scaledWidth;
         Point[] ImagePixels = new Point[4];
 
-        for (int i = 0;i<ImagePixels.length;i++)
-        {
+        for (int i = 0; i < ImagePixels.length; i++) {
             //get the distance from the left and top of the image bounds
             int scaledImageOffsetX = ScreenPoints[i].x - imageBounds.left;
             int scaledImageOffsetY = ScreenPoints[i].y - imageBounds.top;
@@ -223,22 +231,21 @@ public class CropImage extends Activity
             double originalImageOffsetX = scaledImageOffsetX * widthRatio;
             double originalImageOffsetY = scaledImageOffsetY * heightRatio;
 
-            ImagePixels[i] = new Point(originalImageOffsetX,originalImageOffsetY);
+            ImagePixels[i] = new Point(originalImageOffsetX, originalImageOffsetY);
         }
 
         return ImagePixels;
     }
 
-    private void saveOutput()
-    {
+    private void saveOutput() {
         String[] path = imagePath.split("/");
-        String fname = path[path.length-1];
-        File file = new File (ActivityTags.TEMP_STORAGE_DIR, fname);
-        if (file.exists ()) file.delete ();
+        String fname = path[path.length - 1];
+        File file = new File(ActivityTags.TEMP_STORAGE_DIR, fname);
+        if (file.exists())
+            file.delete();
         try {
             FileOutputStream out = new FileOutputStream(file);
-            if (warpedBitmap == null)
-            {
+            if (warpedBitmap == null) {
                 warpedBitmap = origBitmap;
             }
             warpedBitmap.compress(Bitmap.CompressFormat.JPEG, 60, out);
@@ -251,31 +258,28 @@ public class CropImage extends Activity
 
     }
 
-    private class AsyncTaskWarp extends AsyncTask<Bitmap,Void,Bitmap>
-    {
+    private class AsyncTaskWarp extends AsyncTask<Bitmap, Void, Bitmap> {
         Point[] points = new Point[4];
-        LinearLayout progressIndicatorGroup = (LinearLayout)findViewById(R.id.progressIndicatorGroup);
+        LinearLayout progressIndicatorGroup = (LinearLayout) findViewById(R.id.progressIndicatorGroup);
+
         @Override
-        protected Bitmap doInBackground(Bitmap... origImage)
-        {
-            Bitmap newBitmap = warp(origImage[0],points[0],points[1],points[3],points[2]);
-            Log.d (ActivityTags.TAG_LOG,"Do in background done");
+        protected Bitmap doInBackground(Bitmap... origImage) {
+            Bitmap newBitmap = warp(origImage[0], points[0], points[1], points[3], points[2]);
+            Log.d(ActivityTags.TAG_LOG, "Do in background done");
             return newBitmap;
         }
 
         @Override
-        protected void onPreExecute()
-        {
+        protected void onPreExecute() {
             super.onPreExecute();
             progressIndicatorGroup.setVisibility(View.VISIBLE);
             points = ScreenPoint2ImagePixels(EdgeCorners.getPoints());
-            Log.d (ActivityTags.TAG_LOG,"Pre execute done");
+            Log.d(ActivityTags.TAG_LOG, "Pre execute done");
         }
 
         @Override
-        protected void onPostExecute(Bitmap warpedImage)
-        {
-            Log.d (ActivityTags.TAG_LOG,"post execute started");
+        protected void onPostExecute(Bitmap warpedImage) {
+            Log.d(ActivityTags.TAG_LOG, "post execute started");
             super.onPostExecute(warpedImage);
             warpedBitmap = warpedImage;
             progressIndicatorGroup.setVisibility(View.INVISIBLE);
@@ -289,12 +293,9 @@ public class CropImage extends Activity
         }
 
         @Override
-        protected void onCancelled()
-        {
+        protected void onCancelled() {
             super.onCancelled();
         }
     }
-
-
 
 }
